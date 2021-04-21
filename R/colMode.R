4853047d4e0d1e9dedbbae986eb3446b4124a81b
Jacob909 <77281541+Jacob909@users.noreply.github.com>
2021-04-21 15:16:58 -0400

    Add files via upload

---
+++ R/colMode.R
@@ -0,0 +1,16 @@
+#' Return the mode of a column of a data set.
+#'
+#' @param x A data frame column.
+#' @return The mode of column \code{y}.
+#' @examples
+#' colMode(mtcars$cyl)
+#' @export
+colMode <- function(x){
+  tbl <- table(mtcars$wt)
+  index <- which.max(tbl)
+  val <- tbl[index]
+  val2 <- data.frame(val)
+  return(rownames(va2))
+}
+
+
